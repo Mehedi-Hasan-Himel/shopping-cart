79a8f1f78a532cff682ba311ab61f53c0b852fb5
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-02-25 15:44:19 +0600

    first commit

---
+++ js/case.js
@@ -0,0 +1,38 @@
+/* 
+1. addEventListener to the case plus button
+2. get the value inside the case number field (input field)
+3. convert the value into a intiger number.
+4. add 1 to the number
+5. set the value of the input field to the new number
+
+
+*/
+
+function updateCaseNumber(isIncrease) {
+  const caseNumberField = document.getElementById("case-number-field");
+  const caseNumberString = caseNumberField.value;
+  const previousCaseNumber = parseInt(caseNumberString);
+
+  let newCaseNumber;
+  if (isIncrease == true) {
+    newCaseNumber = previousCaseNumber + 1;
+  }
+  if (isIncrease == false && previousCaseNumber > 0) {
+    newCaseNumber = previousCaseNumber - 1;
+  }
+  caseNumberField.value = newCaseNumber;
+  return newCaseNumber;
+}
+
+document.getElementById("btn-case-plus").addEventListener("click", function () {
+  const newCaseNumber = updateCaseNumber(true);
+  const caseTotalPrice = newCaseNumber * 59;
+  const caseTotalElement = document.getElementById("case-total");
+  caseTotalElement.innerText = caseTotalPrice;
+});
+
+document
+  .getElementById("btn-case-minus")
+  .addEventListener("click", function () {
+    updateCaseNumber(false);
+  });
